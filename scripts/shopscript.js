9a03d1ab27c68d96ced4e273254a37f0b1d274e4
FeedbackFox <fox@feedbackfox.xyz>
2019-06-27 15:09:13 +0200

    Testrapports toegevoegd. SEO gefixt.

--- scripts/shopscript.js
+++ scripts/shopscript.js
@@ -13,5 +13,17 @@
             " - $" + $( ".slider-range" ).slider( "values", 1 ) );
     } );
     
-
+    $(document).ready(function() {
+        $(window).scroll(function() {
+            const target = document.querySelectorAll('.foxparallax');
+    
+            for(let i = 0; i < target.length; i++) {
+                var pos = window.pageYOffset * target[i].dataset.parallaxrate;
+    
+                target[i].style.transform = 'translate3d(0px, ' + pos +'px, 0px)'
+            }
+    
+        });
+    });
+    
 	
