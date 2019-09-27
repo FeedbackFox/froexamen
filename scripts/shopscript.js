b5a416c52762fc3583c4e83f1f550ce55bc3d575
Feedback Fox <you@example.com>
2019-09-27 16:50:51 +0200

    Actual filter commit. Didn't commit last time, for some reason.

--- scripts/shopscript.js
+++ scripts/shopscript.js
@@ -1,18 +1,5 @@
-$(function () {
-    $(".slider-range").slider({
-        range: true,
-        min: 0,
-        max: 200,
-        values: [0, 200],
-        slide: function (event, ui) {
-            $(".pricerange-amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-        }
-    });
-    $(".pricerange-amount").val("$" + $(".slider-range").slider("values", 0) +
-        " - $" + $(".slider-range").slider("values", 1));
-});
-
 $(document).ready(function () {
+    //parallax
     $(window).scroll(function () {
         const target = document.querySelectorAll('.foxparallax');
 
@@ -23,9 +10,66 @@ $(document).ready(function () {
         }
 
     });
-});
 
-//Filters
-$(document).ready(function () {
-    
+    function sliderCreator() {
+        $(".slider-range").slider({
+            range: true,
+            min: 0,
+            max: 200,
+            values: [0, 200],
+            slide: function (event, ui) {
+                $(".pricerange-amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+            }
+        });
+        $(".pricerange-amount").val("$" + $(".slider-range").slider("values", 0) +
+            " - $" + $(".slider-range").slider("values", 1));
+    }
+    sliderCreator();
+
+
+
+    let allgames = $(".shop-item");
+    let selectedfilters = [];
+
+
+    //Filter the checkboxes
+
+    let checkboxes = $("input[type=checkbox]");
+    checkboxes.change(function () { //check if checkboxes are changed
+        if ($(this).is(':checked')) {
+            allgames.hide();
+            selectedfilters.push(this.id.slice(0, -7));
+            for (let i = 0; i < selectedfilters.length; i++) {
+                $("." + selectedfilters[i]).show();
+            }
+            filter();
+        } else {
+            selectedfilters.splice(selectedfilters.indexOf(this.id.slice(0, -7)));
+            if (selectedfilters[0]) {
+                allgames.hide();
+                for (let i = 0; i < selectedfilters.length; i++) {
+                    $("." + selectedfilters[i]).show();
+                }
+                filter();
+            } else {
+                allgames.show();
+                filter();
+            }
+        }
+    });
+
+    //Filter by price
+    function filter() {
+        let lowestprice = document.getElementById("pricerange-amount").value.slice(0, 4);
+        let highestprice = document.getElementById("pricerange-amount").value.slice(4);
+        highestprice = highestprice.replace(/\D/g,'');
+        lowestprice = lowestprice.replace(/\D/g,'');
+        for(let i = 1; i < allgames.length; i++) {
+            let price = allgames[i].querySelector('.item-price').innerHTML.replace(/\D/g,'');
+            console.log(price);
+        }
+    }
+
+
+
 });
