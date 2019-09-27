8352348696fdbdbd43779ebdb3b9b8e06a3fe11c
Feedback Fox <you@example.com>
2019-09-27 20:16:05 +0200

    Fixed price slider.

--- scripts/shopscript.js
+++ scripts/shopscript.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
         $(".slider-range").slider({
             range: true,
             min: 0,
-            max: 200,
-            values: [0, 200],
+            max: 250,
+            values: [0, 250],
             slide: function (event, ui) {
                 $(".pricerange-amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
             }
@@ -62,14 +62,45 @@ $(document).ready(function () {
     function filter() {
         let lowestprice = document.getElementById("pricerange-amount").value.slice(0, 4);
         let highestprice = document.getElementById("pricerange-amount").value.slice(4);
-        highestprice = highestprice.replace(/\D/g,'');
-        lowestprice = lowestprice.replace(/\D/g,'');
-        for(let i = 1; i < allgames.length; i++) {
-            let price = allgames[i].querySelector('.item-price').innerHTML.replace(/\D/g,'');
-            console.log(price);
+        highestprice = highestprice.replace(/\D/g, '');
+        lowestprice = lowestprice.replace(/\D/g, '');
+        for (let i = 0; i < allgames.length; i++) {
+            let price = allgames[i].querySelector('.item-price').innerHTML.replace(/\D/g, '');
+            price = parseInt(price);
+
+
+            if (!(price >= lowestprice && price <= highestprice)) {
+                allgames[i].style.display = "none";
+                console.log("poof");
+            }
+            //if the current game doesnt fit in the pricerange, remove it
+            //if it does, execute below code
+            else if (selectedfilters[0]) {
+
+                let elementClasses = $(allgames[i]).attr("class").split(" ");
+                console.log(elementClasses);
+                for (let j = 0; j < elementClasses.length; j++) {
+                    if (selectedfilters.includes(elementClasses[j])) {
+                        console.log(allgames[i]);
+                        allgames[i].style.display = "block";
+                    }
+                }
+
+            } else {
+                console.log("unpoof");
+                allgames[i].style.display = "block";
+            }
+
         }
     }
 
+    //onclick events for the slider
 
+    $('.ui-slider-handle').click(function () {
+        filter();
+    });
+    $('.ui-slider-handle').mouseout(function () {
+        filter();
+    });
 
 });
